a5f8e6d42dea6eff915d12a076e5ff9401a3f515
Daniel Rector <rector.danielk@gmail.com>
2017-07-16 22:59:04 -0400

    imported images to use in table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import blank from './icons/blank.gif';
+import folder from './icons/folder.gif';
+import movie from '.icons/folder.gif';
 
 class App extends Component {
   render() {
@@ -8,7 +11,11 @@ class App extends Component {
         <h1>Index of /</h1>
         <table>
           <tbody>
-            <tr></tr>
+            <tr>
+              <th valign="top">
+                <img src={blank} alt="[ICO]"></img>
+              </th>
+            </tr>
           </tbody>
         </table>
         <address>Apache/2.4.10 (Debian) Server at ns641378.ovh.net Port 80</address>
