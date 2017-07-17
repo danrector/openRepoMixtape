a5867490c0265000937c415cfc9de630bbdea86e
Daniel Rector <rector.danielk@gmail.com>
2017-07-16 22:44:05 -0400

    removed bullshit. added index, and address. removed styling. starting to add table

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Index of /</h1>
+        <table>
+          <tbody>
+            <tr></tr>
+          </tbody>
+        </table>
+        <address>Apache/2.4.10 (Debian) Server at ns641378.ovh.net Port 80</address>
       </div>
+
     );
   }
 }
