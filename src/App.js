fa3a7d9f432056ed18cdc970747cf3617e310a23
Daniel Rector <rector.danielk@gmail.com>
2017-07-17 00:09:39 -0400

    changed server name

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ class App extends Component {
             </tr>
           </tbody>
         </table>
-        <address>Apache/2.4.10 (Debian) Server at ns641378.ovh.net Port 80</address>
+        <address>Apache/2.4.10 (Debian) Server at brutality.herokuapp.com Port 80</address>
       </div>
 
     );
