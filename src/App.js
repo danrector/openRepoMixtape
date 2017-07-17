2d9ea248fd129a4083f96139473ac2dead5144a4
Daniel Rector <rector.danielk@gmail.com>
2017-07-16 23:54:39 -0400

    fixed table. added mock data. fixed styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import blank from './icons/blank.gif';
 import folder from './icons/folder.gif';
-import movie from '.icons/folder.gif';
+import movie from './icons/movie.gif';
+
+var vAlignTop = {
+  verticalAlign: 'top'
+};
+
+var alignRight = {
+  textAlign: 'right'
+};
 
 class App extends Component {
   render() {
@@ -12,9 +20,37 @@ class App extends Component {
         <table>
           <tbody>
             <tr>
-              <th valign="top">
+              <th style={vAlignTop}>
                 <img src={blank} alt="[ICO]"></img>
               </th>
+              <th>
+                <a href="?C=N;O=D">Name</a>
+              </th>
+              <th>
+                <a href="?C=M;O=A">Last modified</a>
+              </th>
+              <th>
+                <a href="?C=S;O=A">Size</a>
+              </th>
+              <th>
+                <a href="?C=D;O=A">Description</a>
+              </th>
+            </tr>
+            <tr>
+              <th colSpan="5">
+                <hr />
+              </th>
+            </tr>
+            <tr>
+              <td style={vAlignTop}>
+                <img src={movie} alt="[VID]" />
+              </td>
+              <td>
+                <a href="">Movie Film Title.mp4</a>
+              </td>
+              <td style={alignRight}>2017-04-28 16:45  </td>
+              <td style={alignRight}>2.0G</td>
+              <td>&nbsp;</td>
             </tr>
           </tbody>
         </table>
