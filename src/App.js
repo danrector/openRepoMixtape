55ebe0fc990488dd5d8171dd7eb2021d3bea69bf
Daniel Rector <rector.danielk@gmail.com>
2017-07-30 14:59:13 -0400

    added working links

--- src/App.js
+++ src/App.js
@@ -58,16 +58,64 @@ class App extends Component {
                 <img src={movie} alt="[VID]" />
               </td>
               <td>
-                <a href="">Movie Film Title.mp4</a>
+                <a href="https://www.dropbox.com/s/6lix6i65sl0o1ih/hard%20jazz.mp3?dl=0">hard jazz.mp3</a>
               </td>
-              <td style={alignRight}>2017-04-28 16:45  </td>
-              <td style={alignRight}>2.0G</td>
+              <td style={alignRight}>2016-06-28 16:32  </td>
+              <td style={alignRight}>8.42MB</td>
               <td>&nbsp;</td>
             </tr>
+
+            {
+              /* second entry */
+            }
+            <tr>
+              <td style={vAlignTop}>
+                <img src={movie} alt="[VID]" />
+              </td>
+              <td>
+                <a href="https://www.dropbox.com/s/oqdsenlle5rowsf/interpolate.mp3?dl=0">interpolate.mp3</a>
+              </td>
+              <td style={alignRight}>2016-06-11 16:37  </td>
+              <td style={alignRight}>12.62MB</td>
+              <td>&nbsp;</td>
+            </tr>
+
+            {
+              /* third entry */
+            }
+            <tr>
+              <td style={vAlignTop}>
+                <img src={movie} alt="[VID]" />
+              </td>
+              <td>
+                <a href="https://www.dropbox.com/s/dxk3n9sya4ee2jh/jame.mp3?dl=0">jame.mp3</a>
+              </td>
+              <td style={alignRight}>2016-06-08 00:58  </td>
+              <td style={alignRight}>6.83MB</td>
+              <td>&nbsp;</td>
+            </tr>
+
+            {
+              /* fourth entry */
+            }
+            <tr>
+              <td style={vAlignTop}>
+                <img src={movie} alt="[VID]" />
+              </td>
+              <td>
+                <a href="https://www.dropbox.com/s/327lvezrsvoev3z/surf%20jam.mp3?dl=0">surf jam.mp3</a>
+              </td>
+              <td style={alignRight}>2016-06-08 00:56  </td>
+              <td style={alignRight}>10.27MB</td>
+              <td>&nbsp;</td>
+            </tr>
+
           </tbody>
         </table>
         <address>Apache/2.4.10 (Debian) Server at brutality.herokuapp.com Port 80</address>
       </div>
+
+
 
     );
   }
